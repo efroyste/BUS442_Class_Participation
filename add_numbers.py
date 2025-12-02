e7ed24c95b242ff98b459ef9bebbe54ed20cf3b9
efroyste <efroyste@ncsu.edu>
2025-12-01 23:32:07 -0500

    Add files via upload

---
+++ add_numbers.py
@@ -0,0 +1,5 @@
+import utils
+
+result = utils.add_num(10,8)
+
+print(f"Result: {result}")
---
+++ utils.py
@@ -0,0 +1,7 @@
+def add_num(a,b):
+    return a+b
+    
+if __name__ == "__main__":
+
+    result = add_num(5,3)
+    print(f"Result: {result}")
